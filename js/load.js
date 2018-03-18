82f67befc836df29f8c5481288fa2091b2ba3f94
HariPota <pixel123@wp.pl>
2018-03-18 23:20:56 +0100

    update

--- js/load.js
+++ js/load.js
@@ -1,18 +1,18 @@
 var loadState = {
 
     preload: function () {
-        game.load.image('bg', '../img/bg.png');
-        game.load.image('frog', '../img/frog.png');
-        game.load.image('car1', '../img/car1.png');
-        game.load.image('car2', '../img/car2.png');
-        game.load.image('carR1', '../img/carR1.png');
-        game.load.image('carR2', '../img/carR2.png');
-        game.load.image('carG1', '../img/carG1.png');
-        game.load.image('carG2', '../img/carG2.png');
-        game.load.image('log', '../img/log.png');
-        game.load.audio("Music", "../mp3/music.wav");
-        game.load.audio("lose", "../mp3/lose.mp3");
-        game.load.audio("win", "../mp3/win.mp3");
+        game.load.image('bg', 'img/bg.png');
+        game.load.image('frog', 'img/frog.png');
+        game.load.image('car1', 'img/car1.png');
+        game.load.image('car2', 'img/car2.png');
+        game.load.image('carR1', 'img/carR1.png');
+        game.load.image('carR2', 'img/carR2.png');
+        game.load.image('carG1', 'img/carG1.png');
+        game.load.image('carG2', 'img/carG2.png');
+        game.load.image('log', 'img/log.png');
+        game.load.audio("Music", "mp3/music.wav");
+        game.load.audio("lose", "mp3/lose.mp3");
+        game.load.audio("win", "mp3/win.mp3");
 
 
 
